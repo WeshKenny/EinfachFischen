1fd4278fbe9b359f5dfa68c6981d552620db9734
WeshKenny <noeheimgartner@yahoo.com>
2025-10-29 14:44:55 +0100

    Feat: made some quality of life changes that make the website cleaner

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Details } from './details/details';
+import { About } from './about/about';
 
 export const routes: Routes = [
-  { path: '', component: Home }  // 👈 Home ist jetzt Startseite
+  { path: '', component: Home },              // Default route (home page)
+  { path: 'details', component: Details },    // /details route
+  { path: 'about', component: About },        // /about route
+  { path: '**', redirectTo: '' }              // Redirect any unknown route to home
 ];
-
